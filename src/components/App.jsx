48bc7cc7ad08932ff641cefc0a2faa49ad56462c
Sergii <svbulgakov75@gmail.com>
2023-07-17 14:10:29 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import  ImageGallery  from "./ImageGallery/ImageGallery";
+import ImageGallery  from "./ImageGallery/ImageGallery";
 import Button from './Button/Button';
 import Modal from './Modal/Modal';
 import Loader from './Loader/Loader';
